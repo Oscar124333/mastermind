3310c845ad3c79d2b136f469c260380eb87f7cd7
Chi Sieu Huynh <160776538+Oscar124333@users.noreply.github.com>
2026-03-31 18:47:59 +0000

    added more functions + digit_counter + good_guess (WIP)

--- main.c
+++ main.c
@@ -7,26 +7,35 @@
 // Constants
 const int testCode[] = {1, 3, 3, 5};
 const int SIZEOFCODE = 4;
+const char SPECIAL_NULL = '#';
 
 // Prototypes
 int *int_to_list(int original);
-int inputHandler(int *variable);
-void inputPrompt(int *variable, char *prompt);
+bool good_guess(char *guess);
+int digit_counter(int integer);
+int inputHandler(char *variable);
+void inputPrompt(char *variable, char *prompt);
 
 int main(void)
 {    
     // Intro
-    printf("\n*****\nWelcome to Mastermindle!\n*****\n");
+    printf("\n*****\nWelcome to Mastermindle!\n*****\n\n");
 
-    int userInput = 0;
-    inputPrompt(&userInput, "Please enter a guess for the secret code. It can only be 4 digits long.");
+    char userInput[5] = {'\0'};
+    inputPrompt(userInput, "Please enter a guess for the secret code. It can only be 4 digits long.");
 
-    int *userGuess = int_to_list(userInput);
+    int userGuess = atoi(userInput);
 
+    printf("%d\n", userGuess);
+
+    int *f_userGuess = int_to_list(userGuess);
+
+    // debug
     for (int i = 0; i < SIZEOFCODE; i++)
     {
-        printf("%d\n", userGuess[i]);
+        printf("%d\n", f_userGuess[i]);
     }
+    // end
 
     return 0;
 }
@@ -43,17 +52,40 @@ int *int_to_list(int original)
     return ptr;
 }
 
+bool good_guess(char *guess)
+{
+    if (guess[4] != SPECIAL_NULL)
+    {
+        return false;
+    }
+    
+    for (int i = 0; i != SPECIAL_NULL; i++)
+    {
+        // how to differentiate the end vs in the middle
+    }
+    return true;
+}
 
-int inputHandler(int *variable)
+int digit_counter(int integer)
+{
+    int counter = 0;
+    for (; integer != 0; counter++)
+    {
+        integer /= 10;
+    }
+    return counter;
+}
+
+int inputHandler(char *variable)
 {
     int status = 0;
     int ch;
-    status = scanf("%d", variable);
+    status = scanf("%s", variable);
     while ((ch = getchar()) != '\n' && ch != EOF);
     return status;
 }
 
-void inputPrompt(int *variable, char *prompt)
+void inputPrompt(char *variable, char *prompt)
 {
     do
     {
